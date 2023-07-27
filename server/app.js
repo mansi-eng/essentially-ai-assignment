cc4a7a5fccce119bf97111f1cb1a944d7420fb88
mansi-eng <paunikarmansi@gmail.com>
2023-07-27 17:08:33 +0530

    Logic to fetch data from Polygon

--- server/app.js
+++ server/app.js
@@ -1,34 +1,48 @@
 // DO NOT MODIFY ANYTHING HERE, THE PLACE WHERE YOU NEED TO WRITE CODE IS MARKED CLEARLY BELOW
 
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const bodyParser = require("body-parser");
 const axios = require("axios");
 
 const app = express();
 
 app.use(function (req, res, next) {
-    const allowedOrigins = ['http://localhost:3000'];
-    const origin = req.headers.origin;
-    if (allowedOrigins.includes(origin)) {
-        res.setHeader('Access-Control-Allow-Origin', origin);
-    }
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    res.header("Access-Control-Allow-credentials", true);
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
-    next();
+  const allowedOrigins = ["http://localhost:3000"];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header("Access-Control-Allow-credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+  next();
 });
 
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 app.use(bodyParser.json());
 
-app.enable('trust proxy');
+app.enable("trust proxy");
 
-app.post('/api/fetchStockData', (req, res) => {
-    // YOUR CODE GOES HERE, PLEASE DO NOT EDIT ANYTHING OUTSIDE THIS FUNCTION
-    res.sendStatus(200);
+app.post("/api/fetchStockData", async (req, res) => {
+  try {
+    const { date, ticker } = req.body;
+
+    // Fetch data from polygon Api
+    const { data } = await axios.get(
+      `https://api.polygon.io/v1/open-close/${ticker}/${date}?adjusted=true&apiKey=${process.env.POLYGON_API_KEY}`
+    );
+    res.status(200).json({ message: "Data fetched successfully", data });
+  } catch (err) {
+    res.status(400).json({ err });
+  }
 });
 
 const port = process.env.PORT || 5000;
