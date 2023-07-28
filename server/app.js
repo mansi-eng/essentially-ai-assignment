e90abc213ff14d6444b7640ea9be5cbd83cfa8bb
mansi-eng <paunikarmansi@gmail.com>
2023-07-28 11:01:32 +0530

    Handle errors in backend more effectively

--- server/app.js
+++ server/app.js
@@ -9,42 +9,59 @@ const cors = require("cors");
 const app = express();
 
 app.use(function (req, res, next) {
-  const allowedOrigins = ["http://localhost:3000"];
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.header("Access-Control-Allow-credentials", true);
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
-  next();
+    const allowedOrigins = ["http://localhost:3000"];
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader("Access-Control-Allow-Origin", origin);
+    }
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    res.header("Access-Control-Allow-credentials", true);
+    res.header(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, UPDATE"
+    );
+    next();
 });
 
 app.use(cors());
 app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
+    bodyParser.urlencoded({
+        extended: true,
+    })
 );
 app.use(bodyParser.json());
 
 app.enable("trust proxy");
 
 app.post("/api/fetchStockData", async (req, res) => {
-  try {
-    const { date, ticker } = req.body;
+    try {
+        // Extract required fields from req body
+        const { date, ticker } = req.body;
 
-    // Fetch data from polygon Api
-    const { data } = await axios.get(
-      `https://api.polygon.io/v1/open-close/${ticker}/${date}?adjusted=true&apiKey=${process.env.POLYGON_API_KEY}`
-    );
-    res.status(200).json({ message: "Data fetched successfully", data });
-  } catch (err) {
-    res.status(400).json({ err });
-  }
+        // Fetch data from polygon Api
+        const { data } = await axios.get(
+            `https://api.polygon.io/v1/open-close/${ticker}/${date}?adjusted=true&apiKey=${process.env.POLYGON_API_KEY}`
+        );
+
+        // Extract required fields
+        const { open, high, low, close, volume } = data;
+        res.status(200).json({
+            message: "Data fetched successfully",
+            data: { open, high, low, close, volume },
+        });
+    } catch (err) {
+        if (err.response) {
+            // The request was made and the server responded with a status code
+            const { status, data } = err.response;
+            res.status(status).json({ error: data });
+        } else {
+            // Something else went wrong
+            res.status(500).json({ error: "An unexpected error occurred" });
+        }
+    }
 });
 
 const port = process.env.PORT || 5000;
