4730d88f5bab602de9e50d45bab11461df956790
mansi-eng <paunikarmansi@gmail.com>
2023-07-28 09:19:27 +0530

    Handle loading state

--- client/src/components/TickerForm.jsx
+++ client/src/components/TickerForm.jsx
@@ -3,44 +3,49 @@ import TickerDetails from "./TickerDetails";
 import axios from "axios";
 
 const TickerForm = () => {
-  const [ticker, setTicker] = useState("");
-  const [date, setDate] = useState("");
-  const [tickerData, setTickerData] = useState();
+    const [ticker, setTicker] = useState("");
+    const [date, setDate] = useState("");
+    const [tickerData, setTickerData] = useState();
+    const [isLoading, setIsLoading] = useState(false);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const body = JSON.stringify({
-      ticker,
-      date,
-    });
-    const { data } = await axios.post(
-      "http://localhost:5000/api/fetchStockData",
-      body,
-      { headers: { "Content-Type": "application/json" } }
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        setIsLoading(true);
+        const body = JSON.stringify({
+            ticker,
+            date,
+        });
+        const { data } = await axios.post(
+            "http://localhost:5000/api/fetchStockData",
+            body,
+            { headers: { "Content-Type": "application/json" } }
+        );
+        setTickerData(data.data);
+        setIsLoading(false);
+    };
+
+    return (
+        <>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    name="ticker-name"
+                    id="ticker-name"
+                    onChange={(e) => setTicker(e.target.value)}
+                />
+                <input
+                    type="date"
+                    name="ticker-date"
+                    id="ticker-date"
+                    onChange={(e) => setDate(e.target.value)}
+                />
+                <input type="submit" value="submit" />
+            </form>
+            {isLoading && <div>Loading....</div>}
+            {!isLoading && tickerData && <TickerDetails data={tickerData} />}
+        </>
     );
-    setTickerData(data.data);
-  };
-
-  return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="ticker-name"
-          id="ticker-name"
-          onChange={(e) => setTicker(e.target.value)}
-        />
-        <input
-          type="date"
-          name="ticker-date"
-          id="ticker-date"
-          onChange={(e) => setDate(e.target.value)}
-        />
-        <input type="submit" value="submit" />
-      </form>
-      {tickerData && <TickerDetails data={tickerData} />}
-    </>
-  );
 };
 
 export default TickerForm;
